336d5b95eebdedb9ee875255673fdb5d5b5aa2c9
ISRAEL N. GBATI <fernandogbat@hotmail.com>
2024-08-21 13:07:43 +0100

    Add files via upload

---
+++ Chapter12-SPI/Debug/Src/adxl345.d
@@ -0,0 +1,19 @@
+Src/adxl345.o: ../Src/adxl345.c ../Inc/adxl345.h ../Inc/spi.h \
+ C:/Users/Ninsaw/Documents/tempWorkspace/Chapter12-SPI/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/Ninsaw/Documents/tempWorkspace/Chapter12-SPI/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ C:/Users/Ninsaw/Documents/tempWorkspace/Chapter12-SPI/chip_headers/CMSIS/Include/core_cm4.h \
+ C:/Users/Ninsaw/Documents/tempWorkspace/Chapter12-SPI/chip_headers/CMSIS/Include/cmsis_version.h \
+ C:/Users/Ninsaw/Documents/tempWorkspace/Chapter12-SPI/chip_headers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/Ninsaw/Documents/tempWorkspace/Chapter12-SPI/chip_headers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Ninsaw/Documents/tempWorkspace/Chapter12-SPI/chip_headers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/Ninsaw/Documents/tempWorkspace/Chapter12-SPI/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+../Inc/adxl345.h:
+../Inc/spi.h:
+C:/Users/Ninsaw/Documents/tempWorkspace/Chapter12-SPI/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/Users/Ninsaw/Documents/tempWorkspace/Chapter12-SPI/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+C:/Users/Ninsaw/Documents/tempWorkspace/Chapter12-SPI/chip_headers/CMSIS/Include/core_cm4.h:
+C:/Users/Ninsaw/Documents/tempWorkspace/Chapter12-SPI/chip_headers/CMSIS/Include/cmsis_version.h:
+C:/Users/Ninsaw/Documents/tempWorkspace/Chapter12-SPI/chip_headers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/Ninsaw/Documents/tempWorkspace/Chapter12-SPI/chip_headers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/Ninsaw/Documents/tempWorkspace/Chapter12-SPI/chip_headers/CMSIS/Include/mpu_armv7.h:
+C:/Users/Ninsaw/Documents/tempWorkspace/Chapter12-SPI/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
---
+++ Chapter12-SPI/Debug/Src/subdir.mk
@@ -0,0 +1,42 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (12.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/adxl345.c \
+../Src/main.c \
+../Src/spi.c \
+../Src/syscalls.c \
+../Src/sysmem.c \
+../Src/uart.c 
+
+OBJS += \
+./Src/adxl345.o \
+./Src/main.o \
+./Src/spi.o \
+./Src/syscalls.o \
+./Src/sysmem.o \
+./Src/uart.o 
+
+C_DEPS += \
+./Src/adxl345.d \
+./Src/main.d \
+./Src/spi.d \
+./Src/syscalls.d \
+./Src/sysmem.d \
+./Src/uart.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DNUCLEO_F411RE -DSTM32 -DSTM32F4 -DSTM32F411RETx -DSTM32F411xE -c -I../Inc -I"C:/Users/Ninsaw/Documents/tempWorkspace/Chapter12-SPI/chip_headers/CMSIS/Include" -I"C:/Users/Ninsaw/Documents/tempWorkspace/Chapter12-SPI/chip_headers/CMSIS/Device/ST/STM32F4xx/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Src
+
+clean-Src:
+	-$(RM) ./Src/adxl345.cyclo ./Src/adxl345.d ./Src/adxl345.o ./Src/adxl345.su ./Src/main.cyclo ./Src/main.d ./Src/main.o ./Src/main.su ./Src/spi.cyclo ./Src/spi.d ./Src/spi.o ./Src/spi.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su ./Src/uart.cyclo ./Src/uart.d ./Src/uart.o ./Src/uart.su
+
+.PHONY: clean-Src
+
