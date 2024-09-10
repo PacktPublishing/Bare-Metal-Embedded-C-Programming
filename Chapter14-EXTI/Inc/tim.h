3c5d6103aa7518ca1924e90488844f51946e471a
ISRAEL N. GBATI <fernandogbat@hotmail.com>
2024-09-10 17:12:03 +0100

    Delete Chapter14-EXTI directory

--- Chapter14-EXTI/Inc/tim.h
+++
@@ -1,8 +0,0 @@
-#include "stm32f4xx.h"
-#ifndef TIM_H_
-#define TIM_H_
-
-#define SR_UIF  (1U<<0)
-void tim2_1hz_init(void);
-
-#endif
--- Chapter14-EXTI/Inc/uart.h
+++
@@ -1,8 +0,0 @@
-#ifndef __UART_H__
-#define __UART_H__
-
-#include "stm32f4xx.h"
-
-void uart_init(void);
-
-#endif
